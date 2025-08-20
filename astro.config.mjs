781ef8647966f612199b2a9f1aa6733986e096e6
sonam0296 <sonam43jha@gmail.com>
2025-08-20 21:48:16 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,68 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "DSA Notes",
+          items: [
+            { label: "Array Patterns", slug: "dsa/array-patterns" },
+            { label: "Two Sum", slug: "dsa/two-sum" },
+            {
+              label: "Best Time to Buy and Sell Stock",
+              slug: "dsa/best-time-to-buy-or-sell-stocks",
+            },
+          ],
+        },
+        {
+          label: "React",
+          items: [
+            {
+              label: "Hooks",
+              items: [
+                { label: "", slug: "react/hooks/react-hooks" },
+
+                // {
+                //   label: "useCallback",
+                //   slug: "react/hooks/use-callback",
+                // },
+              ],
+            },
+            {
+              label: "Interview Questions",
+              items: [
+                {
+                  label: "Class vs Functional Components ",
+                  slug: "react/interviews/class-vs-functional-comp",
+                },
+                {
+                  label: "useState vs useRef vs useReducer",
+                  slug: "react/interviews/usestate-useref-usereducer",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
